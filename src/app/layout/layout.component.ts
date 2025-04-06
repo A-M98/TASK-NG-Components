d23fe62e0f40e7626128310e29722a85cc4b3e1e
A-M98 <aalmohammad@pifss.gov.kw>
2025-04-06 11:17:19 +0300

    Ahmed Mohammed & Abdullah Al-Kharji

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SideBarComponentComponent } from '../side-bar-component/side-bar-component.component';
+import { ContentComponent } from '../content/content.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, SideBarComponentComponent, ContentComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
---
+++ src/app/side-bar-component/side-bar-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-component',
+  imports: [],
+  templateUrl: './side-bar-component.component.html',
+  styleUrl: './side-bar-component.component.css'
+})
+export class SideBarComponentComponent {
+
+}
